68a47e28e79fcfb4ff37ae5b3c2c55c0dcc61573
Melvin Perbis <melperbz5425@gmail>
2025-07-26 22:36:50 -0400

    update routes.py

--- service/routes.py
+++ service/routes.py
@@ -70,7 +70,7 @@ def list_accounts():
     """
     app.logger.info("Request to list Accounts")
     accounts = Account.all()
-    account_list = [account.serialize() for account in accounts]   
+    account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
